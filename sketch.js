e47d770ebbe152677a905a5af48704769cfb212b
GremiTiger032 <bgremi1@lsu.edu>
2024-12-12 20:38:49 -0600

    Final

---
+++ sketch.js
@@ -0,0 +1,61 @@
+function setup() {
+    createCanvas(400, 400);
+    background(213, 124);
+}
+
+function draw() {
+
+
+
+for(var i=5; i < 1000; i = i + 15){
+fill(51)
+ellipse(i, 10, 10);
+ellipse(i, 30, 6);
+ellipse(i, 125, 6);
+ellipse(i, 150, 7);
+ellipse(i, 223, 11);
+ellipse(i, 249, 13);
+ellipse(i, 325, 6);
+ellipse(i, 350, 17);
+fill(123, 1)
+ellipse(i, 50, 14);
+ellipse(i,75, 6);
+ellipse(i, 75, 6);
+fill(51)
+ellipse(i, 100, 18);
+ellipse(i, 50, 4);
+fill(123, 1)
+ellipse(i, 150, 22);
+fill(51)
+ellipse(i, 200, 26);
+fill(123, 1)
+ellipse(i, 250, 30);
+fill(51)
+ellipse(i, 300, 34);
+fill(123, 1)
+ellipse(i, 350, 38);
+fill(341, 432, 432, 413)
+ellipse(i, 100, 5);
+ellipse(i, 174, 9);
+ellipse(i, 199, 11);
+ellipse(i, 275, 10);
+ellipse(i, 300, 15);
+ellipse(i, 10, 3);
+ellipse(i, 375, 3);
+fill(51)
+ellipse(i, 400, 42);
+fill(123, 1)
+ellipse(i, 450, 46);
+fill(341, 432, 432, 413)
+ellipse(i, 400, 21);
+fill(51)
+ellipse(i, 450, 25);
+ellipse(i, 510, 55);
+
+rotate(25, 26)}
+
+
+       
+
+
+}
